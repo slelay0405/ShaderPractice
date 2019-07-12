1e2655c506d88cfe75713a2e9b9d55534d4e3c5b
王巍然 <45957086@qq.com>
2019-07-12 14:36:29 +0800

    上传多光源前向渲染

---
+++ ShaderPractice/Assets/Scripts/Company_ForwardBaseRendering.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Company_ForwardBaseRendering : MonoBehaviour
+{
+
+	public Transform directionalLight;
+	public Transform pointLight;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		directionalLight.Rotate(Vector3.up,4f * Time.deltaTime);
+		
+	}
+}
